edd2b5cf66e6fb0f9051bed6c6ecb80e9806ccb2
DylanV <dverrezen@gmail.com>
2016-05-31 15:19:36 +0200

    Added quick sort

--- algorithms/sort.h
+++ algorithms/sort.h
@@ -44,4 +44,35 @@ void selection_sort(vector<T> &unsorted) {
     }
 }
 
+template <typename T>
+size_t partition(vector<T> &A, size_t lo, size_t hi) { //https://en.wikipedia.org/wiki/Quicksort
+    T pivot = A[lo];
+    size_t i = lo - 1;
+    size_t j = hi + 1;
+    while(true){
+        do{
+            i++;
+        }while(A[i] < pivot);
+        do{
+            j--;
+        }while(A[j] > pivot);
+        if(i >= j){
+            return j;
+        }
+        //swap A[i] and A[j]
+        T temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+}
+
+template <typename T>
+void quick_sort(vector<T> &A, size_t lo, size_t hi) { //https://en.wikipedia.org/wiki/Quicksort
+    if(lo < hi){
+        size_t p = partition(A, lo, hi);
+        quick_sort(A, lo, p);
+        quick_sort(A, p+1, hi);
+    }
+}
+
 #endif //SCRATCH_SORT_H
--- test/test_sort.cpp
+++ test/test_sort.cpp
@@ -7,23 +7,30 @@
 
 using namespace std;
 
-TEST(BubbleSortTest, BubbleSortEvenSize) {
+TEST(SortTest, BubbleSortEvenSize) {
     vector<int> unsorted {4, 6, 1, 3, 9, 8, 2, 5, 10, 7};
     vector<int> sorted {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     bubble_sort(unsorted);
     ASSERT_EQ(sorted, unsorted);
 }
 
-TEST(BubbleSortTest, BubbleSortOddSize) {
+TEST(SortTest, BubbleSortOddSize) {
     vector<int> unsorted {4, 6, 1, 3, 9, 8, 2, 5, 7};
     vector<int> sorted {1, 2, 3, 4, 5, 6, 7, 8, 9};
     bubble_sort(unsorted);
     ASSERT_EQ(sorted, unsorted);
 }
 
-TEST(SelectionSortTest, SelectionSort) {
+TEST(SortTest, SelectionSort) {
     vector<int> unsorted {4, 6, 1, 3, 9, 8, 2, 5, 10, 7};
     vector<int> sorted {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     selection_sort(unsorted);
+    ASSERT_EQ(sorted, unsorted);
+}
+
+TEST(SortTest, QuickSort) {
+    vector<int> unsorted {4, 6, 1, 3, 9, 8, 2, 5, 10, 7};
+    vector<int> sorted {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    quick_sort(unsorted, 0, unsorted.size()-1);
     ASSERT_EQ(sorted, unsorted);
 }
